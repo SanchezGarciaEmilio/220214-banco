992ded8daf7eda3d217005afd91d90db38c37bf0
SanchezGarciaEmilio <esangar2211@g.educaand.es>
2022-02-11 23:31:48 +0100

    Update

--- src/app/components/registrar-personas/registrar-personas.component.ts
+++ src/app/components/registrar-personas/registrar-personas.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ToastrService } from 'ngx-toastr';
-import { Persona, tPersona } from 'src/app/models/clientes';
+import { tPersona } from 'src/app/models/clientes';
 import { ClienteService } from 'src/app/services/clientes.service';
 
 @Component({
